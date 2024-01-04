39e36b483f1b8932ea12d78994150385db938bf0
toliadriapak <toliadriapak@gmail.com>
2024-01-04 14:43:07 +0200

    added new files + clean code

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function () {
+    document.querySelector('.preloader').style.display = 'none';
+  });
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KL0R4XN4BL');
